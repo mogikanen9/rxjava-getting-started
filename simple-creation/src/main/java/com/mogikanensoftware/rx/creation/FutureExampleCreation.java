a302be415dfda669e80d016789f6490771172691
Vladislav V <Vladislav V>
2017-04-24 21:00:47 -0400

    Observable from FutureTask

---
+++ simple-creation/src/main/java/com/mogikanensoftware/rx/creation/FutureExampleCreation.java
@@ -0,0 +1,38 @@
+package com.mogikanensoftware.rx.creation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.FutureTask;
+
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+public class FutureExampleCreation {
+
+	public static void main(String[] args) {
+		
+		FutureTask<List<Integer>> futureTask = new FutureTask<List<Integer>>(() ->{
+			return Arrays.asList(789,90,123);
+		});
+		System.out.println("Future task was created");
+		
+		Observable<List<Integer>> observableFutureList = Observable.from(futureTask);
+		System.out.println("Observable was created");
+		
+		Schedulers.computation().createWorker().schedule(()->{
+			futureTask.run();
+		});
+		System.out.println("Scheduller/Worker was scheduled");
+		
+		
+		observableFutureList.subscribe((list)->{
+			list.forEach((item)->{
+				System.out.println(item);
+			});
+		});
+		System.out.println("Done.");
+		
+		System.exit(0);
+	}
+
+}

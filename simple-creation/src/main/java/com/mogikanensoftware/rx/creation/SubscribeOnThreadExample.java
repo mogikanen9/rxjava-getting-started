9aff1f840b6666576f62ab00be564574ae6c92c6
Vladislav V <Vladislav V>
2017-04-24 21:47:55 -0400

    Simplified
     * includes newThread and io

--- simple-creation/src/main/java/com/mogikanensoftware/rx/creation/SubscribeOnThreadExample.java
+++ simple-creation/src/main/java/com/mogikanensoftware/rx/creation/SubscribeOnThreadExample.java
@@ -9,37 +9,24 @@ import rx.schedulers.Schedulers;
 public class SubscribeOnThreadExample {
 
 	public static void main(String[] args) {
-		Object waitMonitor = new Object();
+		System.out.println(String.format("Currrent thread ->%s", Thread.currentThread().getName()));
 
-		synchronized (waitMonitor) {
+		List<Integer> fibonacciList = Arrays.asList(1, 2, 3, 5, 8, 13, 21, 34);
 
-			List<Integer> fibonacciList = Arrays.asList(1, 2, 3, 5, 8, 13, 21, 34);
+		Observable<Integer> observableList = Observable.from(fibonacciList);
 
-			Observable<Integer> observableList = Observable.from(fibonacciList);
+		observableList
+				.subscribeOn(Schedulers.newThread())
+				// .subscribeOn(Schedulers.io())
+				.subscribe((i) -> {
+					System.out.println(i);
+					System.out.println(String.format("Currrent thread ->%s", Thread.currentThread().getName()));
+				}, (t) -> {
+					t.printStackTrace();
+				}, () -> {
+					System.out.println("Complete");
+				});
 
-			observableList.subscribeOn(Schedulers.newThread()).subscribe((i) -> {
-				System.out.println(i);
-				System.out.println(String.format("Currrent thread ->%s", Thread.currentThread().getName()));
-			}, (t) -> {
-				t.printStackTrace();
-			}, () -> {
-				System.out.println("Complete");
-				synchronized (waitMonitor) {
-					waitMonitor.notify(); //notify that eveyrthing is over
-				}
-			});
-		}
-
-		//wait for monitor
-		synchronized (waitMonitor) {
-			try {
-				waitMonitor.wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		// System.exit(0);
+		System.exit(0);
 	}
 }

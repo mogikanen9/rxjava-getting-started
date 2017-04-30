0afdf457b4f016f0b6c3870cc6b6c2670799d7e4
Vladislav V <Vladislav V>
2017-04-30 19:43:10 -0400

    io scheduler was specified
    waitMonitor was introduced to guarantee that Observable work will be
    complete before app exit

--- simple-composition/src/main/java/com/mogikanensoftware/rx/composition/CompositionExampleApp.java
+++ simple-composition/src/main/java/com/mogikanensoftware/rx/composition/CompositionExampleApp.java
@@ -4,36 +4,48 @@ import com.mogikanensoftware.rx.composition.bean.SecurityRole;
 import com.mogikanensoftware.rx.composition.service.UserService;
 
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 public class CompositionExampleApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		UserService userService = new UserService();
+		Object waitMonitor = new Object();
 
-		System.out.println(Thread.currentThread().getName());
-		System.out.println("Filtering Power Users only...");
-		System.out.println("Power Users");
+		synchronized (waitMonitor) {
+			UserService userService = new UserService();
 
-		Observable.from(userService.fetchAllUsers()).filter((user) -> {
-			return user.getRole().equals(SecurityRole.PowerUser);
-		}).map((user) -> {
-			return user.getEmail();
-		}).subscribe((email) -> {
-			System.out.println(email);
-			// emulate heavy processing
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}, (t) -> {
-			System.out.println("Smth. went wrong->" + t.getMessage());
-		}, () -> {
-			System.out.println("Done processing.");
-		});
+			System.out.println(Thread.currentThread().getName());
+			System.out.println("Filtering Power Users only...");
+			System.out.println("Power Users");
 
-		System.out.println("Done.");
+			Observable.from(userService.fetchAllUsers()).filter((user) -> {
+				return user.getRole().equals(SecurityRole.PowerUser);
+			}).map((user) -> {
+				return user.getEmail();
+			}).subscribeOn(Schedulers.io()).subscribe((email) -> {
+				System.out.println(email);
+				// emulate heavy processing
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}, (t) -> {
+				System.out.println("Smth. went wrong->" + t.getMessage());
+			}, () -> {
+				System.out.println("Done processing.");
+				synchronized (waitMonitor) {
+					waitMonitor.notifyAll();
+				}
+				
+			});
+
+			System.out.println("Done in Main.");
+			
+			waitMonitor.wait();
+		}
+
 		System.exit(0);
 
 	}

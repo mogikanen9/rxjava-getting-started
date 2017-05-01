5e9275845f09e2b36ceb9f0eecd444fc4784324e
Vladislav V <Vladislav V>
2017-04-30 20:58:31 -0400

    Sample for first and firstorDefault mathods
    one more fix for Fib Gen - changed to Long

--- simple-filtering/src/main/java/com/mogikanensoftware/rx/filtering/PositionalExample.java
+++ simple-filtering/src/main/java/com/mogikanensoftware/rx/filtering/PositionalExample.java
@@ -8,13 +8,23 @@ public class PositionalExample {
 
 	public static void main(String[] args) {
 
-		System.out.println("First 6 numbers form Fib Seq");
+		System.out.println("First 6 numbers from Fib Seq");
 		Observable.from(FibonacciGenerator.generateSequence(20)).take(7).subscribe((i) -> {
 			System.out.print(String.format(" %d", i));
 		});
 		
-		System.out.println("\nLast 4 numbers form Fib Seq");
+		System.out.println("\nLast 4 numbers from Fib Seq");
 		Observable.from(FibonacciGenerator.generateSequence(20)).takeLast(4).subscribe((i) -> {
+			System.out.print(String.format(" %d", i));
+		});
+		
+		System.out.println("\nFirst from Fib Seq");
+		Observable.from(FibonacciGenerator.generateSequence(20)).first().subscribe((i) -> {
+			System.out.print(String.format(" %d", i));
+		});
+		
+		System.out.println("\nFirst or default from empty");
+		Observable.empty().firstOrDefault(26L).subscribe((i) -> {
 			System.out.print(String.format(" %d", i));
 		});
 
--- simple-filtering/src/main/java/com/mogikanensoftware/rx/util/FibonacciGenerator.java
+++ simple-filtering/src/main/java/com/mogikanensoftware/rx/util/FibonacciGenerator.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class FibonacciGenerator {
 
-	public static List<Integer> generateSequence(int j) {
-		List<Integer> result = new ArrayList<>();
+	public static List<Long> generateSequence(int j) {
+		List<Long> result = new ArrayList<>();
 		if (j < 0) {
 			throw new RuntimeException(String.format("Invalid parameter n=%d", j));
 		} else if (j == 0) {
 			System.out.println("Empty sequence");
 		} else if (j == 1) {
-			result.add(0);
+			result.add(0L);
 		} else if (j == 2) {
-			result.addAll(Arrays.asList(0, 1));
+			result.addAll(Arrays.asList(0L, 1L));
 		} else {
-			result.addAll(Arrays.asList(0, 1));
+			result.addAll(Arrays.asList(0L, 1L));
 			for (int i = 2; i < j; i++) {
-				int val = result.get(i - 1) + result.get(i - 2);
+				long val = result.get(i - 1) + result.get(i - 2);
 				result.add(val);
 			}
 		}

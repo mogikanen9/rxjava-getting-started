f7a13f01df0986cd75a9c56e9022b79de22c9af7
Vlad <mogikanensoftware@gmail.com>
2017-07-04 21:30:28 -0400

    Workable timeout sample

--- simple-utility/src/main/java/com/mogikanensoftware/rx/utility/FibonacciGenerator.java
+++ simple-utility/src/main/java/com/mogikanensoftware/rx/utility/FibonacciGenerator.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class FibonacciGenerator {
 
 	public static List<Long> generateSequence(int j) {
+		System.out.println(
+				String.format("Generating Fib sequence for %d in Thread->%s", j, Thread.currentThread().getName()));
 		List<Long> result = new ArrayList<>();
 		if (j < 0) {
 			throw new RuntimeException(String.format("Invalid parameter n=%d", j));
@@ -20,6 +22,15 @@ public class FibonacciGenerator {
 			result.addAll(Arrays.asList(0L, 1L));
 			for (int i = 2; i < j; i++) {
 				long val = result.get(i - 1) + result.get(i - 2);
+				if (val == 377) {
+					System.out.println(
+							String.format("Sleep for 3 seconds in Thread->%s", Thread.currentThread().getName()));
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 				result.add(val);
 			}
 		}
--- simple-utility/src/main/java/com/mogikanensoftware/rx/utility/TimeoutExample.java
+++ simple-utility/src/main/java/com/mogikanensoftware/rx/utility/TimeoutExample.java
@@ -1,25 +1,41 @@
 package com.mogikanensoftware.rx.utility;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 public class TimeoutExample {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		System.out.println("Example with timeout");
-		Observable.from(FibonacciGenerator.generateSequence(20)).timeout(1, TimeUnit.SECONDS).subscribe((el) -> {
-			System.out.println(String.format("%d was printed, press ENTER to conitnue", el));
-			try {
-				System.in.read();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		Callable<List<Long>> callableFib = new Callable<List<Long>>() {
+
+			@Override
+			public List<Long> call() throws Exception {
+				return FibonacciGenerator.generateSequence(20);
 			}
-		});
-		
+		};
+
+		System.out.println("Example with timeout");
+		Observable.fromCallable(callableFib).subscribeOn(Schedulers.newThread())
+		//.timeout(2, TimeUnit.SECONDS) //uncomment to get TimeoutException
+		.timeout(5, TimeUnit.SECONDS)
+				.subscribe((el) -> {
+					System.out.println(el);
+				});
+
+		System.out.println("Yo-ho-ho and a botle of rum!");
+		try {
+			System.out.println(String.format("Press ENTER to continue (Thread->%s)",Thread.currentThread().getName()));
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		System.exit(0);
 
 	}
